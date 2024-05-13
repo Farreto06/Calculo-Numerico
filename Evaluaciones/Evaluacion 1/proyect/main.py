fdfc9aa93fc9278f695f074ccc1d3181b23281e3
Farrauto <jvfarrauto06@gmail.com>
2024-05-13 17:26:04 -0400

    add virtual env to proyect 1

---
+++ Evaluaciones/Evaluacion 1/proyect/main.py
@@ -0,0 +1,18 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title="Menu"
+    page.window_height=400
+    page.window_width=350
+    page.vertical_alignment=ft.MainAxisAlignment.CENTER
+
+    page.theme_mode=ft.ThemeMode.LIGHT
+
+    text=ft.Text(value="Seleciona la operacion a Realizar",text_align=ft.TextAlign.CENTER,width=350)
+    b_Sis_Num=ft.TextButton(text="Sistemas Númericos")
+    b_Gauss=ft.TextButton(text="Reduccion Gausiana")
+
+    page.add(ft.Column([text,b_Sis_Num,b_Gauss]))
+
+ft.app(main)
